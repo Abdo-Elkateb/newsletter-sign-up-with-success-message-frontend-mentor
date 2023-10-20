1d6d176a7838b62d8bb4527d74931dfa02b5dea5
Abdo Elkateb <93152380+Abdo-Elkateb@users.noreply.github.com>
2023-10-20 02:18:08 +0200

    js

--- app.js
+++ app.js
@@ -1,46 +1,41 @@
 // js 
 let email = document.getElementById('email')
-let subscribe = document.getElementById('subscribe');
 let emailArror = document.getElementById('email-arror');
-console.log(emailArror)
+let newsletter = document.getElementById('newsletter');
+let body = document.getElementById('body');
+
+let result = document.getElementById('result');
 
 
 
-s
+
+
 function checkMessage() {
 
-let email = document.getElementById('email')
-    if(!email.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)){
+    let email = document.getElementById('email')
+    if (!email.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
         emailArror.innerHTML = "valid email required"
+        email.style.background = "hsl(4, 89%, 93%)"
         email.style.color = "hsl(4, 100%, 67%)"
+        email.style.border = "1px solid hsl(4, 100%, 67%)"
+        
+
         return false
+    } else {
+        emailArror.innerHTML = ""
+        email.style.background = "#fff"
+        email.style.border = "1px solid  hsl(235, 18 %, 26 %)"
+        return true
     }
-    emailArror.innerHTML = ""
-    email.style.background = "#fff"
-    return true
 }
 
-// function submitMessage() {
-//     subscribe.addEventListener('click', function() {
-//         checkMessage()
-//     })
+// let subscribe = document.getElementById('subscribe').addEventListener('click', function() {
+//     sendForm()
+// })
+function sendForm() {
+    result.style.display = 'block'
+    newsletter.style.display = 'none'
+    checkMessage()
 
-
-// }
-// submitMessage()
-
-// const validate = () => {
-//     const $result = ('#result');
-//     $result.text('');
-
-//     if (validateEmail(email)) {
-//         email.text(email + ' is valid.');
-//         // $result.css('color', 'green');
-//     } else {
-//         email.text(email + ' is invalid.');
-//         // $result.css('color', 'red');
-//     }
-// }
-
-// $('#email').on('input', validate);
+}
 
