7109b1b953fd1f1c71ce90da682af510f47e97a2
Abdo Elkateb <93152380+Abdo-Elkateb@users.noreply.github.com>
2023-10-22 20:52:07 +0200

    fix problem comes from input

--- app.js
+++ app.js
@@ -1,41 +1,76 @@
 // js 
-let email = document.getElementById('email')
+// let email = document.getElementById('email')
+let email = document.forms["Form"]["answer_a"];
 let emailArror = document.getElementById('email-arror');
 let newsletter = document.getElementById('newsletter');
 let body = document.getElementById('body');
 
 let result = document.getElementById('result');
+let subscribe = document.getElementById('subscribe')
 
 
 
+// this function check email 
+function checkEmail() {
 
 
-function checkMessage() {
-
-    let email = document.getElementById('email')
-    if (!email.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
-        emailArror.innerHTML = "valid email required"
-        email.style.background = "hsl(4, 89%, 93%)"
-        email.style.color = "hsl(4, 100%, 67%)"
-        email.style.border = "1px solid hsl(4, 100%, 67%)"
-        
-
-        return false
-    } else {
+    if (email.value.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+        // if the input was a right email,  you will get this style
         emailArror.innerHTML = ""
         email.style.background = "#fff"
-        email.style.border = "1px solid  hsl(235, 18 %, 26 %)"
+        email.style.border = "1px solid var(--Grey)"
+        email.style.color = "var(--Grey)"
         return true
+    }
+    else {
+
+        // if the input was wrong email,  you will get this style
+
+        emailArror.innerHTML = "valid email required"
+        email.style.background = "hsl(4, 89%, 93%)"
+        email.style.color = "var(--tomato)"
+        email.style.border = "1px solid var(--tomato)"
+        return false
     }
 }
 
-// let subscribe = document.getElementById('subscribe').addEventListener('click', function() {
-//     sendForm()
-// })
+// after click 
+
 function sendForm() {
     result.style.display = 'block'
     newsletter.style.display = 'none'
-    checkMessage()
+}
+
+//  here we can click  👍
+function subscribeClick() {
+    subscribe.addEventListener('click', function () {
+        //  here we call funtion 
+        if (checkEmail() === true) {
+            sendForm()
+            rest() 
+        } else {
+            emailArror.innerHTML = "write your email"
+            rest() 
+    
+        }
+
+
+    })
 
 }
+subscribeClick()
+
+
+
+
+//  here we rest the value comes from email 
+
+function rest() {
+    email.value = ""
+}
+
+
+
+
+
 
